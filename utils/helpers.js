ccd8eda81806466d0460f61e3fe619596d787df5
Part-time-Dan <126934952+Part-time-Dan@users.noreply.github.com>
2023-07-27 21:41:18 -0600

    seed data, not done yet

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,2 +1,8 @@
 // put the date fns (or other package stuff here)
 
+module.exports = {
+    format_date: (date) => {
+      // Format date as MM/DD/YYYY
+      return date.toLocaleDateString();
+    },
+};
