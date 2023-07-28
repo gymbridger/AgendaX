47d5f371ab9a3aa16bdc6e75c5e46ded14a9da08
Ryan Mattson <ryan.mattson32@gmail.com>
2023-07-27 18:54:00 -0600

    More seeds.js code

--- seeds/seed.js
+++ seeds/seed.js
@@ -4,5 +4,10 @@ const {User} = require('../models');
 const userData = require('./user.json')
 
 const seedDatabase = async () => {
+    await sequelize.sync({ force: true});
 
+    await User.bulkCreate(userData, {
+        individualHooks: true,
+        returning: true,
+    });
 };
