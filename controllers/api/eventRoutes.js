1b8723e3b0241a6a102ae70fc043e902f2c25c41
Ryan Mattson <ryan.mattson32@gmail.com>
2023-07-31 19:20:16 -0600

    Commit

--- controllers/api/eventRoutes.js
+++ controllers/api/eventRoutes.js
@@ -1,38 +1,19 @@
 const router = require('express').Router();
+const sequelize = require('sequelize');
 const { Event } = require('../../models');
 const withAuth = require('../../utils/auth');
 
 router.post('/', withAuth, async (req, res) => {
-  try {
-    const newEvent = await Event.create({
-      ...req.body,
-      user_id: req.session.user_id,
-    });
-
-    res.status(200).json(newEvent);
+  try {const eventData = await Event.create({
+    name: req.body.name,
+    description: req.body.description,
+    due_date: req.body.due_date,
+    user_id: req.session.userId
+  });
+  eventData = eventData.get({plain: true});
+    res.send(eventData)
   } catch (err) {
-    res.status(400).json(err);
-  }
-});
-
-router.delete('/:id', withAuth, async (req, res) => {
-  try {
-    const eventData = await Event.destroy({
-      where: {
-        id: req.params.id,
-        user_id: req.session.user_id,
-      },
-    });
-
-    if (!eventData) {
-      res.status(404).json({ message: 'No event found with this id!' });
-      return;
-    }
-
-    res.status(200).json(eventData);
-  } catch (err) {
+    console.log(err);
     res.status(500).json(err);
   }
 });
-
-module.exports = router;
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,4 +1,6 @@
+const { User, Event, Date } = require('../models');
 const router = require('express').Router();
 const withAuth = require('../utils/auth');
-
+const sequelize = require('sequelize');
 module.exports = router;
+
