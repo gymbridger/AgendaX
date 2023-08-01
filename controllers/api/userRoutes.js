dc3fc2e1c7b87c81f4468570f3e1394159b8a4f4
Ryan Mattson <ryan.mattson32@gmail.com>
2023-07-31 18:49:49 -0600

    UserRoutes.js Create and add id and username

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -2,19 +2,24 @@ const router = require('express').Router();
 const sequelize = require('sequelize');
 const { User, Event, Date} = require('../../models');
 
-router.post('/', async (req, res) =>{
+router.post('/', async (req, res) => {
     try {
-        const dbUserData = await User.create({
-            username: req.body.username,
-            email: req.body.email,
-            password: req.body.pssword,
-        });
-
-        req.session.save(() => {
-            req.session.loggedIn = true;
-            req.session.userId = dbUserData.get({plain:true}).id;
-          res.status(200).end();
-        });
-    }
-})
-module.exports = router;
+      const dbUserData = await User.create({
+        first_name: req.body.firstName,
+        last_name: req.body.lastName,
+        email: req.body.email,
+        password: req.body.password,
+      });
+      // adds id and username to the session so that they can be displayed
+      req.session.save(() => {
+        req.session.loggedIn = true;
+        req.session.userId  = dbUserData.get({plain:true}).id;
+        req.session.firstName  = dbUserData.get({plain:true}).first_name;
+        res.status(200).end();
+      });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+      }
+    });  
+module.exports = router; 
