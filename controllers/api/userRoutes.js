22227d8886edc5696b776148edef6c484b1b150a
Part-time-Dan <126934952+Part-time-Dan@users.noreply.github.com>
2023-07-31 21:11:46 -0600

    my change commit

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -2,23 +2,37 @@ const router = require('express').Router();
 const sequelize = require('sequelize');
 const { User } = require('../../models');
 
-router.post('/', async (req, res) =>{
+router.post('/login', async (req, res) => {
     try {
-        const dbUserData = await User.create({
-            username: req.body.username,
-            email: req.body.email,
-            password: req.body.pssword,
-        });
-
-        req.session.save(() => {
-            req.session.loggedIn = true;
-            req.session.userId = dbUserData.get({plain:true}).id;
-          res.status(200).end();
-        });
+      const userData = await User.findOne({ where: { username: req.body.username } });
+  
+      if (!userData) {
+        res
+          .status(400)
+          .json({ message: 'Incorrect username, please try again' });
+        return;
+      }
+      console.log(userData)
+  
+      const validPassword = userData.checkPassword(req.body.password);
+  
+      if (!validPassword) {
+        res
+          .status(400)
+          .json({ message: 'Incorrect password, please try again' });
+        return;
+      }
+  
+      req.session.save(() => {
+        req.session.user_id = userData.id;
+        req.session.logged_in = true;
+        
+        res.json({ user: userData, message: 'You are now logged in!' });
+      });
+  
     } catch (err) {
-        console.log('Error')
-        res.status(400).json(err);
+      res.status(400).json(err);
     }
-});
+  });
 
 module.exports = router;
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,9 +2,7 @@ const router = require('express').Router();
 const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
-
       res.render('homepage');
-
   });
   
 
