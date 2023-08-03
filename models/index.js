1be80c3acf146fa21fae622881b74a75c6e97c25
Ryan Mattson <ryan.mattson32@gmail.com>
2023-08-02 20:48:02 -0600

    commit

--- models/index.js
+++ models/index.js
@@ -13,18 +13,11 @@ Event.belongsTo(User, {
 });
 
 // many dates belong to 1 user instance. this is a many to one relationship
-Date.belongsTo(User, {
-  foreignKey: "user_id",
-});
-
-Event.hasMany(Date, {
-  foreignKey: "date_id",
-});
 
 Event.hasMany(User, {
   foreignKey: "date_id",
 });
 
-module.exports = { Event, Date, User };
+module.exports = { Event, User };
 
 // When we use "hasMany", we set a foreign key in the "many" model to link back to the "one" model.
