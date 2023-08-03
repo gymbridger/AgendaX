edbfe866e298894b1fd720151b3d51dc77e6a18b
Bridger Daw <bridgerdaw@yahoo.com>
2023-08-02 21:14:58 -0600

    changed quotes

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,48 +1,48 @@
 const newFormHandler = async (event) => {
-    event.preventDefault();
-  
-    const name = document.querySelector('#event-name').value.trim();
-    const startTime = document.querySelector('#start-time').value.trim();
-    const endTime = document.querySelector('#end-time').value.trim();
-    const description = document.querySelector('#event-desc').value.trim();
-  
-    if (name && description && startTime && endTime) {
-      const response = await fetch(`/api/events`, {
-        method: 'POST',
-        body: JSON.stringify({ name, description, starting_date, ending_date }),
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
-  
-      if (response.ok) {
-        document.location.replace('/profile');
-      } else {
-        alert('Failed to create event');
-      }
+  event.preventDefault();
+
+  const name = document.querySelector("#event-name").value.trim();
+  const startTime = document.querySelector("#start-time").value.trim();
+  const endTime = document.querySelector("#end-time").value.trim();
+  const description = document.querySelector("#event-desc").value.trim();
+
+  if (name && description && startTime && endTime) {
+    const response = await fetch(`/api/events`, {
+      method: "POST",
+      body: JSON.stringify({ name, description, startTime, endTime }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (response.ok) {
+      document.location.replace("/profile");
+    } else {
+      alert("Failed to create event");
     }
-  };
-  
-  const delButtonHandler = async (event) => {
-    if (event.target.hasAttribute('data-id')) {
-      const id = event.target.getAttribute('data-id');
-  
-      const response = await fetch(`/api/events/${id}`, {
-        method: 'DELETE',
-      });
-  
-      if (response.ok) {
-        document.location.replace('/profile');
-      } else {
-        alert('Failed to delete event');
-      }
+  }
+};
+
+const delButtonHandler = async (event) => {
+  if (event.target.hasAttribute("data-id")) {
+    const id = event.target.getAttribute("data-id");
+
+    const response = await fetch(`/api/events/${id}`, {
+      method: "DELETE",
+    });
+
+    if (response.ok) {
+      document.location.replace("/profile");
+    } else {
+      alert("Failed to delete event");
     }
-  };
-  
-  document
-    .querySelector('.new-event-form')
-    .addEventListener('submit', newFormHandler);
-  
-  document
-    .querySelector('.event-list')
-    .addEventListener('click', delButtonHandler);
+  }
+};
+
+document
+  .querySelector(".new-event-form")
+  .addEventListener("submit", newFormHandler);
+
+document
+  .querySelector(".event-list")
+  .addEventListener("click", delButtonHandler);
