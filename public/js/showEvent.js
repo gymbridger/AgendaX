bbc236571c78bd8568c7c3f57161c66b81db4ad4
Bridger Daw <bridgerdaw@yahoo.com>
2023-08-03 22:19:25 -0600

    made me change quotes

--- public/js/showEvent.js
+++ public/js/showEvent.js
@@ -1,36 +1,28 @@
-document.addEventListener('click', async (event) => {
-    if (event.target && event.target.matches('#delete-button')) {
-        console.log('Show event Click')
-        try {
-            const confirmed = window.confirm('Are you sure you want to delete this event?');
+document.addEventListener("click", async (event) => {
+  if (event.target && event.target.matches("#delete-button")) {
+    console.log("Show event Click");
+    try {
+      const confirmed = window.confirm(
+        "Are you sure you want to delete this event?"
+      );
 
-            if (confirmed) {
+      if (confirmed) {
+        const eventId = window.location.pathname.split("/").pop();
 
-                const eventId = window.location.pathname.split('/').pop();
+        // Make a DELETE request to delete the event
+        const response = await fetch(`/api/events/${eventId}`, {
+          method: "DELETE",
+        });
 
-                // Make a DELETE request to delete the event
-                const response = await fetch(`/api/events/${eventId}`, {
-                    method: 'DELETE',
-                });
-
-                if (response.ok) {
-
-                    document.location.replace('/profile');
-                } else {
-
-                    alert('Failed to delete event');
-                }
-            }
-        } catch (error) {
-            console.error(error);
-            alert('An error occurred while deleting the event');
+        if (response.ok) {
+          document.location.replace("/profile");
+        } else {
+          alert("Failed to delete event");
         }
+      }
+    } catch (error) {
+      console.error(error);
+      alert("An error occurred while deleting the event");
     }
+  }
 });
-
-  
-  
-  
-  
-  
-  
