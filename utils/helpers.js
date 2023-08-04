8ec2b40c6fb1aef997145814fa8e79070b349c25
Bridger Daw <bridgerdaw@yahoo.com>
2023-08-03 22:19:34 -0600

    added date-fns function

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,8 +1,24 @@
 // put the date fns (or other package stuff here)
 
-module.exports = {
-    format_date: (date) => {
-      // Format date as MM/DD/YYYY
-      return date.toLocaleDateString();
-    },
-};
+// module.exports = {
+//   format_date: (date) => {
+//     // Format date as MM/DD/YYYY
+//     return date.toLocaleDateString();
+//   },
+// };
+
+var formatDistanceToNow = require("date-fns/formatDistanceToNow");
+const formatDistance = require("date-fns/formatDistance");
+
+function eventDetails(event) {
+  const startDate = new Date(event.starting_date);
+  const endDate = new Date(event.ending_date);
+  const timeUntilStart = formatDistanceToNow(startDate);
+  const eventLength = formatDistance(startDate, endDate);
+  return {
+    timeUntilStart,
+    eventLength,
+  };
+}
+
+module.exports = eventDetails;
