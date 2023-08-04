91a70214a4a57aaaf379710c44e1a780bec0b328
Part-time-Dan <126934952+Part-time-Dan@users.noreply.github.com>
2023-08-04 01:13:14 -0600

    corrected merge error, code from unknown push

--- public/js/login.js
+++ public/js/login.js
@@ -3,8 +3,8 @@ async function loginFormHandler(event) {
   event.preventDefault();
 
   // Collect values from the login form
-  const username = document.querySelector("#loginPageUsername").value.trim();
-  const password = document.querySelector("#loginPagePassword").value.trim();
+  const username = document.querySelector('#username-login').value.trim();
+  const password = document.querySelector('#password-login').value.trim();
   console.log("Submitting login form");
 
   if (username && password) {
