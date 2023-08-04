5fc1dc4847e739f526a1fc5033bc8272e9703dbb
Part-time-Dan <126934952+Part-time-Dan@users.noreply.github.com>
2023-08-04 00:42:20 -0600

    delete button js fix, add event route linked to modal buttons

--- public/js/addEvent.js
+++ public/js/addEvent.js
@@ -1,30 +1,36 @@
-document.getElementById('add-event-button').addEventListener('click', async () => {
-    try {
-      // Collect data for the new event (replace with your own data collection logic)
-      const eventData = {
-        name: 'New Event',
-        description: 'This is a new event.',
-        starting_date: '2023-07-30',
-        ending_date: '2023-07-31',
-      };
-  
-      const response = await fetch('/api/events', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(eventData),
-      });
-  
-      if (response.ok) {
-        window.location.replace('/profile');
-      } else {
+document.querySelector('.new-event-form').addEventListener('submit', async (event) => {
+  event.preventDefault();
 
-        alert('Failed to add event');
-      }
-    } catch (error) {
-      console.error(error);
-      alert('An error occurred while adding the event');
+  try {
+    // Collect data for new event
+    const name = document.querySelector('#event-name').value.trim();
+    const startTime = document.querySelector('#start-time').value.trim();
+    const endTime = document.querySelector('#end-time').value.trim();
+    const description = document.querySelector('#event-desc').value.trim();
+
+    // Create the eventData object with the user-provided data
+    const eventData = {
+      name: name,
+      starting_date: startTime,
+      ending_date: endTime,
+      description: description,
+    };
+
+    const response = await fetch('/api/events', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(eventData),
+    });
+
+    if (response.ok) {
+      document.location.replace('/profile');
+    } else {
+      alert('Failed to add event');
     }
-  });
-  
+  } catch (error) {
+    console.error(error);
+    alert('An error occurred while adding the event');
+  }
+});
--- public/js/login.js
+++ public/js/login.js
@@ -1,4 +1,5 @@
-const loginFormHandler = async (event) => {
+// Define the loginFormHandler function
+async function loginFormHandler(event) {
   event.preventDefault();
 
   // Collect values from the login form
@@ -17,38 +18,15 @@ const loginFormHandler = async (event) => {
       // If successful, redirect the browser to the profile page
       document.location.replace('/profile');
     } else {
-      alert(response.statusText);
+      alert('Invalid credentials. Please try again.'); // Display an error message
     }
   }
-};
+}
 
-const loginForm= document.getElementById("login-form");
+document.addEventListener("DOMContentLoaded", function () {
+  const loginForm = document.getElementById("login-form");
 
-loginForm.addEventListener('submit', loginFormHandler);
+  // Attach the named function as the event listener
+  loginForm.addEventListener("submit", loginFormHandler);
+});
 
-// sign up still does not route
-// const signupFormHandler = async (event) => {
-//   event.preventDefault();
-
-//   const name = document.querySelector('#username-signup').value.trim();
-//   const email = document.querySelector('#email-signup').value.trim();
-//   const password = document.querySelector('#password-signup').value.trim();
-
-//   if (name && email && password) {
-//     const response = await fetch('/api/users/login', {
-//       method: 'POST',
-//       body: JSON.stringify({ name, email, password }),
-//       headers: { 'Content-Type': 'application/json' },
-//     });
-
-//     if (response.ok) {
-//       document.location.replace('/profile');
-//     } else {
-//       alert(response.statusText);
-//     }
-//   }
-// };
-
-// document
-//   .querySelector('.signup-form')
-//   .addEventListener('submit', signupFormHandler);
